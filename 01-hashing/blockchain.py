eccd7f499df9ca02a340b7cf4e97b01e32fe1acd
Renato Barros de Lima Freitas <renatobarros@dimap.ufrn.br>
2019-08-15 15:50:21 -0300

    generateHash finalizado

--- 01-hashing/blockchain.py
+++ 01-hashing/blockchain.py
@@ -1,3 +1,5 @@
+import hashlib, json
+
 class Blockchain(object):
 
     @staticmethod
@@ -6,7 +8,8 @@ class Blockchain(object):
         # Confira a documentação do hashlib: https://docs.python.org/3/library/hashlib.html
         # Note que o argumento passado pode ser um objeto, portanto serialize o argumento antes.
         # Dica: Use o json.dumps() do módulo json.
-        pass
+        data_string = json.dumps(data).encode()
+        return hashlib.sha256(data_string).hexdigest()
 
 
 # Testando sua implementação: espera-se um retorno True.
@@ -17,7 +20,7 @@ var1 = {
         }
 expected_hash1 = "4145c81419ee987c94f741936c3277e9b281e2ffc9faa3edb5693128e1ee65c1"
 var1_hash = Blockchain.generateHash(var1)
-print(f'Dados: {var1}')
-print(f'Hash   gerado: {var1_hash}')
-print(f'Hash esperado: {expected_hash1}')
-print(f'Iguais? {expected_hash1==var1_hash}\n')
+print('Dados: {}'.format(var1))
+print('Hash   gerado: {}'.format(var1_hash))
+print('Hash esperado: {}'.format(expected_hash1))
+print('Iguais? {}\n'.format(expected_hash1==var1_hash))
